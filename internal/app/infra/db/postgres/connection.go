c0c73cf5c81fad23fe9b7ede2abd99daaba7fc6d
Артём Зимин <artemzimin@MacBook-Pro-Artem.local>
2026-01-13 13:05:15 +0300

    remove db logger

--- internal/app/infra/db/postgres/connection.go
+++ internal/app/infra/db/postgres/connection.go
@@ -5,13 +5,11 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
 func Connect(cfg *config.Config) (*gorm.DB, error) {
 	db, err := gorm.Open(postgres.Open(cfg.DBString), &gorm.Config{
 		TranslateError: true,
-		Logger:         logger.Default.LogMode(logger.Info),
 	})
 	if err != nil {
 		return nil, err
